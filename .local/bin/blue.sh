2de8e4131cf0b08e68e5f4c5c87221f32e53e3f5
amritxyz <amrit44404@proton.me>
2025-01-10 00:54:46 +0545

    new changes

--- .local/bin/blue.sh
+++ .local/bin/blue.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 get_device_list() {
     bluetoothctl devices | awk -F ' ' '{print $3 " " $2}'
 }
-
 # Display devices using dmenu and get the selected device name
 device_list=$(get_device_list)
 selected_device=$(echo "$device_list" | dmenu -l 10 | awk '{print $1}')
@@ -11,17 +9,14 @@ if [ -z "$selected_device" ]; then
     echo "No device selected."
     exit 1
 fi
-
 # Get the MAC address of the selected device
 MAC=$(echo "$device_list" | grep "$selected_device" | awk '{print $2}')
 if [ -z "$MAC" ]; then
     echo "Failed to find MAC address for selected device."
     exit 1
 fi
-
 # Check the connection status of the selected device
 connect=$(bluetoothctl info "$MAC" | grep Connected: | awk '{print $2}')
-
 # Attempt to connect or disconnect based on the connection status
 if [ "$connect" = "no" ]; then
     echo "Attempting to connect to $selected_device..."
--- .local/bin/firefox.sh
+++ .local/bin/firefox.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Define Firefox directory and profile location
 browserdir="/home/void/.mozilla/firefox"
 profilesini="$browserdir/profiles.ini"
